15f7be6f779de11b552abbfebfeac82bb052c6dc
Ammar Hameed <ammarro444@gmail.com>
2021-11-02 02:49:51 +0500

    Restructuring
    Restructuring  + Enabled Wheel Zoom

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    setTimeout(function(){
+        $(".loader-container").hide();
+     }, 500);
+    if (!$('#myCanvas').tagcanvas({
+        reverse: false,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        wheelZoom: true,
+        outlineMethod: 'none',
+    }, 'tags')) {
+        // something went wrong, hide the canvas container
+        $('#myCanvasContainer').hide();
+    }
+     
+});
